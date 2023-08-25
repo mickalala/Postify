0032b75ca9a0a31c133076554c76edb22cd3b72c
mickalala <mickalarissa01@gmail.com>
2023-08-25 16:28:39 -0300

    health and medias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MediasService } from './medias/medias.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MediasService],
 })
 export class AppModule {}
